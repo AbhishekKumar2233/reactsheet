41b4a605e9372be54fc4b812fa8946ba2a00130f
Abhishek Kumar <67472833+AbhishekKumar2233@users.noreply.github.com>
2022-01-11 00:31:01 +0530

    css

--- src/Task1.js
+++ src/Task1.js
@@ -4,10 +4,15 @@ import "./styles.css";
 const name = "abhishek Kumar";
 const currentDate = new Date();
 const year = currentDate.getFullYear(); //current year function
+//img
+
+const img = "https://picsum.photos/200";
 
 export default function Task1() {
   return (
     <div className="Task1">
+      <img src={img}></img>
+      <img src={img + "?grayscale"}></img>
       <h4>Created by {name}</h4>
       <h5>Copyright {year}</h5>
     </div>
