2d6ae936ab29b268c638b13556810d6b8848b51a
Abhishek Kumar <67472833+AbhishekKumar2233@users.noreply.github.com>
2022-01-14 14:09:35 +0530

    inline css in React

--- src/Task1.js
+++ src/Task1.js
@@ -4,17 +4,26 @@ import "./styles.css";
 const name = "abhishek Kumar";
 const currentDate = new Date();
 const year = currentDate.getFullYear(); //current year function
-//img
 
+//img
 const img = "https://picsum.photos/200";
+
+//inline css using js var
+const copyright = {
+  color: "black",
+  textDecoration: "underline"
+};
+//change style using var
+copyright.color = "grey";
 
 export default function Task1() {
   return (
     <div className="Task1">
       <img src={img}></img>
       <img src={img + "?grayscale"}></img>
-      <h4>Created by {name}</h4>
-      <h5>Copyright {year}</h5>
+      {/* inline css */}
+      <h4 style={{ color: "#5fa726", fontSize: "20px" }}>Created by {name}</h4>
+      <h5 style={copyright}>@Copyright {year}</h5>
     </div>
   );
 }
